cc6d8be55438748e6f8b5dfc04bb7e98aaf39705
Vinh <20520862@gm.uit.edu.vn>
2023-04-14 10:22:38 +0700

    update latest file

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def read_data(file_path):
     
 def solve(values, weights, capacity):
     start_time = time.time()
-    solver = pywrapknapsack_solver.KnapsackSolver(pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, 'Knapsack')
+    solver = pywrapknapsack_solver.KnapsackSolver(pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_SCIP_MIP_SOLVER, 'Knapsack')
     solver.set_time_limit(timeout)
 
     solver.Init(values, [weights], [capacity])
@@ -31,25 +31,33 @@ def solve(values, weights, capacity):
 
 def main():
     global timeout 
-    timeout = 420
+    timeout = 300
     
-    files_path = glob.glob(r'kplib-master\06*\*\*\*.kp')
-    result_path = r'result/06.csv'
+    files_path = glob.glob(r'kplib-master\09*\n0[0-1]*\*\*.kp')
+    result_path = r'SCIP result\09test.csv'
+
+    # Uncomment this code to create new file
     with open(result_path, "w") as f:
-        f.write("filePath,totalValue,totalWeight,processingTime")
-    # i = files_path.index(r'kplib-master\02StronglyCorrelated\n00200\R01000\s022.kp')
+        f.write("class,n,R,s,totalValue,totalWeight,processingTime")
+
+    # Uncomment this code to append existing file
+    # last_file_path =  r'kplib-master\09SpannerStronglyCorrelated\n00500\R10000\s006.kp'
+    # i = files_path.index(last_file_path)
     # files_path = files_path[i+1:]
+
     for file_path in files_path:
         values, weights, capacity = read_data(file_path)
         total_value, total_weight, processing_time = solve(values, weights, capacity)
         
         with open(result_path, 'a') as f:
-            f.write(f'\n{file_path[13:]},{total_value},{total_weight},{processing_time}')
+            path_split = file_path.split('\\')
+            f.write(f'\n{path_split[1]},{path_split[2]},{path_split[3]},{path_split[4]},{total_value},{total_weight},{processing_time}')
     print('Completed')
 
     # values, weights, capacity = read_data(r'kplib-master\00Uncorrelated\n00050\R01000\s094.kp')
     # total_value, total_weight, processing_time = solve(values, weights, capacity)
         
     # print(total_value, total_weight, processing_time)
+
 if __name__ == '__main__':
     main()
